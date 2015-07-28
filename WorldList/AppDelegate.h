e28c4811b88144b9edc4b5f101db2e36674d5b08
Artem Podustov <a.podustov@gmail.com>
2015-07-28 06:25:07 +0300

    Initial Commit

---
+++ WorldList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WorldList
+//
+//  Created by Artem Podustov on 7/28/15.
+//  Copyright (c) 2015 OLEArt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WorldList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WorldList
+//
+//  Created by Artem Podustov on 7/28/15.
+//  Copyright (c) 2015 OLEArt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
